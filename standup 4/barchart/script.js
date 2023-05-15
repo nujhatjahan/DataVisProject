c2e842f61bfb99e4ce292d979773e49d45892d57
nujhatjahan <83401155+nujhatjahan@users.noreply.github.com>
2023-05-16 00:18:29 +1000

    Add files via upload

--- standup 4/barchart/script.js
+++ standup 4/barchart/script.js
@@ -1,30 +1,19 @@
+
 function init() {
     var w = 600;
     var h = 400;
-    var dataset = [
-        { apples: 5, oranges: 10, grapes: 22 },
-        { apples: 4, oranges: 12, grapes: 28 },
-        { apples: 2, oranges: 19, grapes: 32 },
-        { apples: 7, oranges: 23, grapes: 35 },
-        { apples: 23, oranges: 17, grapes: 43 }
-    ];
+    var dataset = [];
 
     // Set up scales
     var xScale = d3.scaleBand()
-        .domain(d3.range(dataset.length))
         .rangeRound([0, w])
         .paddingInner(0.05)
-        .paddingOuter(0.1); // Add padding between groups
+        .paddingOuter(0.1);
 
     var yScale = d3.scaleLinear()
-        .domain([0, d3.max(dataset, function(d) {
-            return d3.max([d.apples, d.oranges, d.grapes]);
-        })])
         .range([h, 0]);
 
-        var colors = d3.scaleOrdinal(d3.schemeDark2);
-
-
+    var colors = d3.scaleOrdinal(d3.schemeDark2);
 
     // Set up the SVG
     var svg = d3.select("#chart")
@@ -32,52 +21,66 @@ function init() {
         .attr("width", w)
         .attr("height", h);
 
-    var groups = svg.selectAll("g")
-        .data(dataset)
-        .enter()
-        .append("g")
-        .attr("transform", function(d, i) {
-            return "translate(" + xScale(i) + ",0)"; // Translate each group
+    d3.csv("top10_dataset.xlsx").then(function(data) {
+        // Process the loaded data
+        dataset = data.map(function(d) {
+            return {
+                country: d.country,
+                a: +d["2010"],
+                b: +d["2015"],
+                c: +d["2020"]
+            };
         });
 
-    var rects = groups.selectAll("rect")
-        .data(function(d) { return Object.values(d); }) // Get an array of values for each group
-        .enter()
-        .append("rect")
-        .attr("x", function(d, i) {
-            return xScale.bandwidth() / 3* i; // Adjust x position within each group
-        })
-        .attr("y", function(d) {
-            return yScale(d);
-        })
-        .attr("height", function(d) {
-            return h - yScale(d);
-        })
-        .attr("width", xScale.bandwidth() / 3) // Adjust width of each bar within a group
-        .style("fill", function(d, i) {
-            return colors(i % 3); // Use modulo operator to repeat colors for each group
-        })
-        .style("opacity", 1)
-        .on("mouseover", function(d) {
-            // Get the bar value
-            var value = d3.select(this).data()[0];
+        // Update scales with the loaded data
+        xScale.domain(d3.range(dataset.length));
+        yScale.domain([0, d3.max(dataset, function(d) {
+            return d3.max([d.a, d.b, d.c]);
+        })]);
 
+        // Draw the chart
+        var groups = svg.selectAll("g")
+            .data(dataset)
+            .enter()
+            .append("g")
+            .attr("transform", function(d, i) {
+                return "translate(" + xScale(i) + ",0)";
+            });
 
-            // Transition effect
-            d3.select(this)
-                .transition()
-                .duration(200)
-                .style("opacity", 0.7);
-        })
-        .on("mouseout", function() {
-            
-
-            // Transition effect
-            d3.select(this)
-                .transition()
-                .duration(200)
-                .style("opacity", 1);
-        });
+        var rects = groups.selectAll("rect")
+            .data(function(d) { return Object.values(d).slice(1); })
+            .enter()
+            .append("rect")
+            .attr("x", function(d, i) {
+                return xScale.bandwidth() / 3 * i;
+            })
+            .attr("y", function(d) {
+                return yScale(d);
+            })
+            .attr("height", function(d) {
+                return h - yScale(d);
+            })
+            .attr("width", xScale.bandwidth() / 3)
+            .style("fill", function(d, i) {
+                return colors(i % 3);
+            })
+            .style("opacity", 1)
+            .on("mouseover", function() {
+                d3.select(this)
+                    .transition()
+                    .duration(200)
+                    .style("opacity", 0.7);
+            })
+            .on("mouseout", function() {
+                d3.select(this)
+                    .transition()
+                    .duration(200)
+                    .style("opacity", 1);
+            });
+    }).catch(function(error) {
+        console.log("Error loading data:", error);
+    });
 }
 
 window.onload = init;
+
